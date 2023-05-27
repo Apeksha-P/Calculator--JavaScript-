0c50d1643413cd733d18dcaec2d20eb1ec5ac720
Apeksha60 <pavithriapeksha1new@gmail.com>
2023-05-27 12:53:05 +0530

    addition, subtraction,multiplication,divition

--- calculator.js
+++ calculator.js
@@ -1,6 +1,26 @@
-function calculate(){
+/*function calculate(){
     //alert("Button test");
     let fn = parseFloat(document.getElementById("txtFirstNumber").value);
     let sn = parseFloat(document.getElementById("txtSecondNumber").value);
     document.getElementById("result").value = fn + sn;
+}*/
+function add(){
+    let fn = parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn = parseFloat(document.getElementById("txtSecondNumber").value);
+    document.getElementById("result").value = fn + sn;
+}
+function sub(){
+    let fn = parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn = parseFloat(document.getElementById("txtSecondNumber").value);
+    document.getElementById("result").value = fn - sn;
+}
+function multiple(){
+    let fn = parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn = parseFloat(document.getElementById("txtSecondNumber").value);
+    document.getElementById("result").value = fn * sn;
+}
+function divide(){
+    let fn = parseFloat(document.getElementById("txtFirstNumber").value);
+    let sn = parseFloat(document.getElementById("txtSecondNumber").value);
+    document.getElementById("result").value = fn / sn;
 }
